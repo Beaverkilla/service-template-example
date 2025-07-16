31ec89ec8a4cbd20eff4dd6feef92f9ae8bfbcad
Jack Lever <jack.lever@hub24.com.au>
2025-07-16 14:27:09 +1000

    Fix swagger https

--- src/main/kotlin/au/com/hub24/servicetemplateexample/ServiceTemplateExample.kt
+++ src/main/kotlin/au/com/hub24/servicetemplateexample/ServiceTemplateExample.kt
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(servers = {@Server(url = "${server.servlet.context-path}", description = "Default Server URL")})
 @SpringBootApplication
 class GroundAllocatorApplication
 
